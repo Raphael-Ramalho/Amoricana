466149d9e0153cbc853678a52387d40e4d93e85b
Raphael-Ramalho <rsramalhovf@gmail.com>
2024-08-16 01:09:13 -0300

    adding navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Radio } from "antd";
 import { MainSection } from "./App.styled";
 
 function App() {
+
   return (
     <MainSection>
       <>ola</>
+
+      <Radio.Group onChange={()=>{}}>
+        <Radio.Button value="top">Horizontal</Radio.Button>
+        <Radio.Button value="left">Vertical</Radio.Button>
+      </Radio.Group>
     </MainSection>
   );
 }
